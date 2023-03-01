affb203269b9b7e710148c05c5b6ca311d7b8487
Chao.Ma11 <chao.ma11@lotuscars.com.cn>
2023-03-01 14:35:38 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,144 @@
+import 'dart:convert';
+
+import 'package:fair/fair.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  // runApp(MyApp());
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FairApp.runApplication(
+    _getApp(),
+    plugins: {},
+  );
+}
+
+dynamic _getApp() => FairApp(
+  modules: {},
+  delegate: {},
+  child: MyApp(),
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FairWidget(
+        /// path 可以是 assets 目录下的 bundle 资源，也可以是手机存储
+        /// 里的 bundle 资源，如果是手机存储里的 bundle 资源需要使用绝对路径
+          path: 'assets/bundle/lib_main.fair.json',
+          data: {
+            /// 此处的 key 必须是 fairProps，不可以自定义
+            /// value 是一个 Map 类型的数据，最好是进行 jsonEncode() 操作
+            'fairProps': jsonEncode({'title': '你好'})
+          }));
+  }
+}
+
+@FairPatch()
+class MyHomePage extends StatefulWidget {
+  MyHomePage({ this.fairProps});
+
+  dynamic fairProps;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @FairProps()
+  var fairProps;
+
+  @override
+  void initState() {
+    super.initState();
+    fairProps = widget.fairProps;
+  }
+
+  String getTitle() {
+    return fairProps['title'];
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(getTitle()),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: 40, color: Color(0xffeb4237), wordSpacing: 0),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
